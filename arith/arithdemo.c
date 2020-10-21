6fd5d9d3d20b7500ceb0b70c80686e220361c7da
YogitaChandrawanshi <72123797+YogitaChandrawanshi@users.noreply.github.com>
2020-10-21 21:20:49 +0530

    Add files via upload

---
+++ arith/arithdemo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include"arith.h"
+int main()
+{
+printf("addition:%d\n",add(10,20));
+printf("subtraction:%d\n",sub(20,10));
+return 0;
+
+}
---
+++ arith/pune/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"arith.h"
+int main(int argc,char const *argv[])
+{
+printf("friend add:%d\n",add(10,20));
+return 0;
+}
