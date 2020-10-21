3c7726121b56b180f28011a1b6d6717f557c9863
YogitaChandrawanshi <72123797+YogitaChandrawanshi@users.noreply.github.com>
2020-10-21 21:27:36 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int add(int a,int b)
+{
+int sum=0;
+sum=a+b;
+return sum;
+
+}
+int main(int argc,char const*argv[])
+{
+printf("program #1\n");
+printf("addition=%d\n",add(10,20));
+return 0;
+
+}
