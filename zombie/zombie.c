497494de48ab0bc21daab403514e54ac58db2b8d
YogitaChandrawanshi <72123797+YogitaChandrawanshi@users.noreply.github.com>
2020-10-29 13:41:16 +0530

    Add files via upload

---
+++ zombie/zombie.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+int main(int argc, const char *argv[])
+{
+
+    pid_t id;
+    id=fork();
+    if(0==id)
+    {
+        printf("child starting\n");
+        sleep(2);
+        printf("child exiting\n");
+    }
+    else
+    {
+      printf("parent starting\n");
+      sleep(12);
+      printf("parent exiting\n");
+
+    }
+}
